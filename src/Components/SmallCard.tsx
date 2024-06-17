72353f9513638f090d783230a451962df205b732
Soprano7 <144703389+Soprano7@users.noreply.github.com>
2024-06-17 17:46:31 +0300

    refactor

--- src/Components/SmallCard.tsx
+++ src/Components/SmallCard.tsx
@@ -1,39 +1,35 @@
-// @flow
-import * as React from 'react';
-import RectangleImage from './../Assests/Rectangle-332.webp'
-import RectangleImageTwo from './../Assests/Rectangle-292.webp'
-import RectangleImageThree from './../Assests/Rectangle-293.webp'
-import RectangleImageFive from './../Assests/Rectangle-333.webp'
-import {CardText} from "./CardText";
+import React, {useRef, useState} from 'react';
+import {Video} from "./Video";
 
-type Props = {
 
-};
+interface SmallCardProps {
+    videoSource1: string;
+    videoSource2: string;
+    videoSource3: string;
+    videoSource4: string;
 
-export const SmallCard = (props: Props) => {
+}
+
+export const SmallCard = (props: SmallCardProps) => {
+    const [play1, setPlay1] = useState(false);
+    const [play2, setPlay2] = useState(false);
+    const [play3, setPlay3] = useState(false);
+    const [play4, setPlay4] = useState(false);
+
+    const videoRef1 = useRef<HTMLVideoElement>(null);
+    const videoRef2 = useRef<HTMLVideoElement>(null);
+    const videoRef3 = useRef<HTMLVideoElement>(null);
+    const videoRef4 = useRef<HTMLVideoElement>(null);
+
     return (
         <div className="smallCard">
             <div className="smallCard-images">
-                <div className="image-container">
-                    <img src={RectangleImage} />
-                    <div className="image-text"><CardText/> </div>
-                </div>
-                <div className="image-container">
-                    <img src={RectangleImageTwo} />
-                    <div className="image-text">
-                       <CardText/>
-                    </div>
-                </div>
-                <div className="image-container">
-                    <img src={RectangleImageThree} />
-                    <div className="image-text"><CardText/></div>
-                </div>
-                <div className="image-container">
-                    <img src={RectangleImageFive} />
-                    <div className="image-text"><CardText/></div>
-                </div>
+                <Video play={play1} setPlay={setPlay1} videoRef={videoRef1} videoSource={props.videoSource1} />
+                <Video play={play2} setPlay={setPlay2} videoRef={videoRef2} videoSource={props.videoSource2}/>
+                <Video play={play3} setPlay={setPlay3} videoRef={videoRef3} videoSource={props.videoSource3}/>
+                <Video play={play4} setPlay={setPlay4} videoRef={videoRef4} videoSource={props.videoSource4}/>
             </div>
-
         </div>
+
     );
 };
